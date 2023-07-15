9fae7ac9a7f40588e15b95c51394d4c2c6507656
gyojink <ksmug1998@naver.com>
2023-07-16 00:25:42 +0900

    distructuring을 이용한 Effect 최적화

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,46 +39,51 @@ const Login = (props) => {
         isValid: null,
     });
 
-    // useEffect(() => {
-    //     console.log("Effect Running");
+    useEffect(() => {
+        console.log("Effect Running");
 
-    //     return () => {
-    //         console.log("Effect Clean up");
-    //     };
-    // }, []);
+        return () => {
+            console.log("Effect Clean up");
+        };
+    }, []);
 
-    // useEffect(() => {
-    //     // 디바운싱 : 사용자가 타이핑을 일시 중지했을 때(입력을 끝냈을 때), 유효성 검사 실시.
-    //     const identifier = setTimeout(() => {
-    //         console.log("Checking form validity");
-    //         setFormIsValid(
-    //             enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    //         );
-    //     }, 500);
+    // 이팩트가 불필요하게 실행되는 것을 방지하기 위해서
+    // 유효성 검사를 수행하는 Reducer변수 emailState와 passwordState의
+    // isValid키 값을 객체 디스트럭처링을 이용하여 상수로 저장하여
+    // 유효성 검사의 결과 변경되지 않으면 이팩트가 불필요하게 실행되지 않도록 함.
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
 
-    //     // useEffect는 "함수"!를 리턴할 수 있음
-    //     // 클린업 함수라고 함
-    //     // 클린업 함수는 모든 새로운 사이드이펙트 함수가 실행되기 전에, 그리고 컴포넌트가 제거되기 전에 실행된다.
-    //     return () => {
-    //         console.log("clean up!");
-    //         clearTimeout(identifier);
-    //     };
-    // }, [enteredEmail, enteredPassword]);
+    useEffect(() => {
+        // 디바운싱 : 사용자가 타이핑을 일시 중지했을 때(입력을 끝냈을 때), 유효성 검사 실시.
+        const identifier = setTimeout(() => {
+            console.log("Checking form validity");
+            setFormIsValid(emailIsValid && passwordIsValid);
+        }, 500);
+
+        // useEffect는 "함수"!를 리턴할 수 있음
+        // 클린업 함수라고 함
+        // 클린업 함수는 모든 새로운 사이드이펙트 함수가 실행되기 전에, 그리고 컴포넌트가 제거되기 전에 실행된다.
+        return () => {
+            console.log("clean up!");
+            clearTimeout(identifier);
+        };
+    }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
         dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-        setFormIsValid(
-            event.target.value.includes("@") && passwordState.isValid
-        );
+        // setFormIsValid(
+        //     event.target.value.includes("@") && passwordState.isValid
+        // );
     };
 
     const passwordChangeHandler = (event) => {
         dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
-        setFormIsValid(
-            emailState.isValid && event.target.value.trim().length > 6
-        );
+        // setFormIsValid(
+        //     emailState.isValid && event.target.value.trim().length > 6
+        // );
     };
 
     const validateEmailHandler = () => {
